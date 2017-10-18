af46d9bf52a4347f1e4db9ee837fe68108763bff
Shisir <netotravels@gmail.com>
2017-10-18 16:50:29 +0000

    avatar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,8 +8,9 @@ class ApplicationController < ActionController::Base
   protected
 
 def configure_permitted_parameters
-  devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :username) }
-  devise_parameter_sanitizer.permit(:sign_in) { |u| u.permit(:email, :password, :remember_me) }
-  devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:email, :password, :password_confirmation, :current_password, :username, :introduction) }
+  devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :username, {avatars: []}) }
+  devise_parameter_sanitizer.permit(:sign_in) { |u| u.permit(:email, :password, :remember_me, {avatars: []}) }
+  devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:email, :password, :password_confirmation, :current_password, :username, :introduction, {avatars: []}) }
+  
 end
 end
