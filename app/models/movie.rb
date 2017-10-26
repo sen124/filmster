dda7bc50fbac7bda003fe4e12393432675ff8532
Shisir <netotravels@gmail.com>
2017-10-26 21:17:15 +0000

    validate

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,6 @@
 class Movie < ActiveRecord::Base
+  validates :title, :plot, :release_date, :released, :runtime, :popularity, 
+            :genre, :language, :budget, :average_vote, :vote_count, :poster, :homepage,
+            :tmdb_id, :imdb_id, presence: true
 end
+
