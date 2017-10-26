23d256e38867eab636760036afe90872e6129ce4
Shisir <netotravels@gmail.com>
2017-10-26 17:12:28 +0000

    displaying movie through search

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,10 +1,16 @@
 /* global $*/
+/*global id*/
 
 $(function(){
+  $("#movies").on('click',".movie_poster",function(e){
+    e.preventDefault();
+    let id=$(e.target).data("id");
+  
+  })
   let form = $('#movie-search');
   form.submit(function(e){
     e.preventDefault();
-
+  
     $.ajax({
       url: 'https://api.themoviedb.org/3/search/movie?api_key=1527c2e276f4fdfe458f89ff6921d36e',
       data: form.serialize()
@@ -15,26 +21,24 @@ $(function(){
   });
 
   function displayMovies(data){
-  let container = $("#movies");
-  let htmlString = "";
-
-  container.empty();
-
-  let imageUrl = getBaseImageUrl();
-
-  if (data["results"].length == 0) {
-    htmlString = `<div class="alert alert-danger text-center" role="alert">No Data Found!</div>`;
-  }
-  else{
-    data["results"].forEach(function(movie){
-      htmlString += `<img src=${movie["poster_path"] == null ? "/assets/your_default_image.png" : imageUrl + "/" + movie["poster_path"]} />
+    let container=$("#movies");
+    let htmlString = "";
+    
+    container.empty();
+    
+    let imageUrl = getBaseImageUrl();
+    if (data["results"].length==0){
+      htmlString = `<div class = "alert alert-danger text-center">No Data Found! </div>`;
+    }
+    else
+     data["results"].forEach(function(movie){
+      htmlString += `<img src= ${movie["poster_path"]==null? "/assets/your_default_image.png" :imageUrl + "/" + movie["poster_path"]} data-id="${movie['id']}" class = "movie_poster"/>
                      <p>${movie["title"]}</p>
                      <p>${movie["overview"]}</p>`;
     });
-  }
 
-  container.append(htmlString);
-}
+    $("#movies").append(htmlString);
+  }
 
   function getBaseImageUrl(){
     var url = "";
