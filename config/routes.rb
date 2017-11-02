43cbb1af5341f962812c9b62d9d4c63c38d71e75
Shisir <netotravels@gmail.com>
2017-11-02 22:43:24 +0000

    reviews routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   
   devise_for :users
   resources :users, only: :show
+  resources :reviews
   root 'staticpages#index'
   
   # The priority is based upon order of creation: first created -> highest priority.
